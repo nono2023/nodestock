daef5613da552d87eb1229e93551f81771d4963b
Arnaud Colle <arnaud.colle@chgestion.com>
2021-04-08 18:17:45 -0300

    using the stock market API

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const express = require('express');
 const app = express();
 const expressHandlebars = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+// API key pk_fe1a2bf32e13467e98612f1048e56a6d
+// create call API function
+function call_api(finishedAPI){
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_fe1a2bf32e13467e98612f1048e56a6d', { json: true}, (err, res, body) => {
+    if (err) { return console.log(err);}
+    if (res.statusCode === 200){
+        finishedAPI(body)
+    };
+});
+};
+
+
+
+
 
 // set handlebars middlewares
 app.engine('handlebars', expressHandlebars());
@@ -15,9 +31,16 @@ const otherstuff = "hello there, this is other stuff!";
 
 //set handlebar routes
 app.get('/', function(req, res) {
-    res.render('home', {
-        stuff: otherstuff
+    call_api(function(doneAPI) {
+            res.render('home', {
+            stock: doneAPI
+        });
     });
+});
+
+// create about page route
+app.get('/about.html', function(req, res) {
+    res.render('about');
 });
 
 // set static folder
